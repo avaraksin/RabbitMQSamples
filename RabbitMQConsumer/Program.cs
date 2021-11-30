0e0a832f006f049ba89e0d36ca2e9eacc30c9747
Viktor <Dzitskiy>
2021-11-30 22:34:47 +0300

    Init

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    internal class Program
+    {
+		static void Main()
+		{
+			using (var connection = GetRabbitConnection())
+			using (var channel = connection.CreateModel())
+			{
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body;
+					var message = Encoding.UTF8.GetString(body.ToArray());
+
+					//throw new Exception("Всё пропало!");
+
+					Console.WriteLine(" [x] Received {0}", message);
+				};
+
+				
+				channel.BasicConsume(queue: "hello",
+									 autoAck: true,
+									 consumer: consumer);
+
+				Console.WriteLine(" Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+
+		static private IConnection GetRabbitConnection()
+		{
+			ConnectionFactory factory = new ConnectionFactory
+			{
+				UserName = "niyvrxot",
+				Password = "NmOSQmD8PLvjH5eX2ovHuqY3OR53BAv9",
+				VirtualHost = "niyvrxot",
+				HostName = "woodpecker.rmq.cloudamqp.com"
+			};
+			IConnection conn = factory.CreateConnection();
+			return conn;
+		}
+
+	}
+}
---
+++ RabbitMQProduser/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQProduser
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			using (var connection = GetRabbitConnection())
+			using (var channel = connection.CreateModel())
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					string message = DateTime.UtcNow.ToString();
+
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(exchange: "",
+										 routingKey: "hello",
+										 basicProperties: null,
+										 body: body);
+					
+					Console.WriteLine($"Отправлено {message}");
+				}
+			}
+
+			Console.WriteLine(" Press [enter] to exit.");
+			Console.ReadLine();
+		}
+
+		static private IConnection GetRabbitConnection()
+		{
+			ConnectionFactory factory = new ConnectionFactory
+			{
+				UserName = "niyvrxot",
+				Password = "NmOSQmD8PLvjH5eX2ovHuqY3OR53BAv9",
+				VirtualHost = "niyvrxot",
+				HostName = "woodpecker.rmq.cloudamqp.com"
+			};
+			IConnection conn = factory.CreateConnection();
+			return conn;
+		}
+	}
+}
